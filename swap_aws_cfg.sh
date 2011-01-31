19bce89c4140aaa6a0d4b7bc36e7a630742f74b7
Nick Dimiduk <ndimiduk@gmail.com>
2011-01-31 09:17:09 -0800

    prepare file for supporting multiple tools

--- swap_aws_cfg.sh
+++ swap_aws_cfg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+S3CFG_BASE="${HOME}/.s3cfg"
+
 MSG="Current selected environment is"
-DELTA=`diff -u ${HOME}/.s3cfg_dev ${HOME}/.s3cfg | wc -l`
+DELTA=`diff -u ${S3CFG_BASE}_dev ${S3CFG_BASE} | wc -l`
 
 if [ 0 -eq $DELTA ]
 then
@@ -12,14 +14,14 @@ fi
 
 if [ 1 -ne "$#" ] || ( [ "$1" != "dev" ] && [ "$1" != "prod" ] )
 then
-    echo "Usage: `basename $0` dev | prod"
+    echo "Usage: `basename $0` [ dev | prod ]"
     echo "$MSG"
     exit 0
 fi
 
 if [ "$1" = "dev" ]
 then
-    cp ${HOME}/.s3cfg_dev ${HOME}/.s3cfg
+    cp ${S3CFG_BASE}_dev ${S3CFG_BASE}
 else
-    cp ${HOME}/.s3cfg_prod ${HOME}/.s3cfg
+    cp ${S3CFG_BASE}_prod ${S3CFG_BASE}
 fi
